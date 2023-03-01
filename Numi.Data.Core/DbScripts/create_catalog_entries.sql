98de4493f66a2861e6989f07489d82721e1c8d27
Martin Holly <martin.holly.33@gmail.com>
2023-02-28 21:30:02 -0500

    Added some basic sql scripts

---
+++ Numi.Data.Core/DbScripts/create_catalog_entries.sql
@@ -0,0 +1,29 @@
+﻿DO $$
+DECLARE _tablename VARCHAR(100) := 'catalog_entries';
+
+BEGIN
+IF EXISTS(SELECT * FROM pg_tables AS T WHERE T.tablename = _tablename) THEN
+	-- Add alter statements
+	RAISE NOTICE 'Table [%] already exists', _tablename;
+	
+ELSE
+	-- Add create statements
+	RAISE NOTICE 'Table [%] doe not exist', _tablename;
+	RAISE NOTICE 'Creating table [%] doe not exist', _tablename;
+
+	CREATE TABLE public.catalog_entries
+    (
+        id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+        CONSTRAINT pk_id PRIMARY KEY (id)
+    )
+
+    TABLESPACE pg_default;
+
+    ALTER TABLE public.catalog_entries
+        OWNER to postgres;
+
+    GRANT ALL ON TABLE public.catalog_entries TO postgres;
+
+END IF;
+
+END $$;
---
+++ Numi.Data.Core/DbScripts/create_users.sql
@@ -0,0 +1,32 @@
+﻿DO $$
+DECLARE _tablename VARCHAR(100) := 'users';
+
+BEGIN
+IF EXISTS(SELECT * FROM pg_tables AS T WHERE T.tablename = _tablename) THEN
+	-- Add alter statements
+	RAISE NOTICE 'Table [%] already exists', _tablename;
+	
+ELSE
+	-- Add create statements
+	RAISE NOTICE 'Table [%] doe not exist', _tablename;
+	RAISE NOTICE 'Creating table [%] doe not exist', _tablename;
+
+	CREATE TABLE public.users
+    (
+        id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+        username character varying(50) COLLATE pg_catalog."default" NOT NULL,
+        password character(48) COLLATE pg_catalog."default" NOT NULL,
+        email character varying(300) COLLATE pg_catalog."default" NOT NULL,
+        CONSTRAINT pk_users_id PRIMARY KEY (id)
+    )
+
+    TABLESPACE pg_default;
+
+    ALTER TABLE public.users
+        OWNER to postgres;
+
+    GRANT ALL ON TABLE public.users TO postgres;
+
+END IF;
+
+END $$;
